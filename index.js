cd14c1c211e034b1dd21de5e788981c20fff1e2c
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-14 02:16:37 -0600

    rendered html from fetch data via a template literal

---
+++ index.js
@@ -0,0 +1,17 @@
+// fetch post from the scrimba api blog
+
+fetch("//apis.scrimba.com/jsonplaceholder/posts")
+.then(res=>res.json())
+.then(data=>{
+  const postsArr=data.slice(0, 5)
+  let html =""
+  for(let post of postsArr) {
+    html += `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <hr/>
+    `
+  }
+    document.getElementById("blog-list").innerHTML = html
+    console.log(postsArr)
+})
