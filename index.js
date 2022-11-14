0cb48d46b21ee2a9ccc3ce47f09aaf01e768ed68
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-14 03:40:34 -0600

    Css for form

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // fetch post from the scrimba api blog
 
 fetch("//apis.scrimba.com/jsonplaceholder/posts")
-.then(res=>res.json())
-.then(data=>{
+.then(res => res.json())
+.then(data => {
   const postsArr=data.slice(0, 5)
   let html =""
   for(let post of postsArr) {
-    html += `
-    <h3>${post.title}</h3>
+    html +=
+     `
+     <h3>${post.title}</h3>
     <p>${post.body}</p>
     <hr/>
+    
     `
   }
     document.getElementById("blog-list").innerHTML = html
